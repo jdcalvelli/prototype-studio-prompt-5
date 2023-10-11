413bf698f4a7ad9e7355611d5c8291c508b9c44d
JD Calvelli <jdcalvelli@10-18-252-205.dynapool.wireless.nyu.edu>
2023-10-11 12:50:41 -0400

    adding door with correct frame

--- source/scenes/PlayState.hx
+++ source/scenes/PlayState.hx
@@ -3,23 +3,32 @@ package scenes;
 import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.FlxState;
+import flixel.graphics.frames.FlxFrame;
 import singletons.GameManager;
 import singletons.Messenger;
 
 class PlayState extends FlxState
 {
-	// static sprites dict
+	// sprites dict
 	var spriteDict:Map<String, FlxSprite> = [
 		"background" => new FlxSprite().loadGraphic(AssetPaths.Background__PNG),
 		"room" => new FlxSprite().loadGraphic(AssetPaths.Room__PNG),
 		"desk" => new FlxSprite().loadGraphic(AssetPaths.Desk__PNG),
 		"stuff-on-desks" => new FlxSprite().loadGraphic(AssetPaths.Stuff_On_Desk__PNG),
 		"whiteboard" => new FlxSprite().loadGraphic(AssetPaths.Whiteboard__PNG),
+		"gameboy" => new FlxSprite().loadGraphic(AssetPaths.gameboy_spritesheet__png, true, 640, 360),
+		"door" => new FlxSprite().loadGraphic(AssetPaths.door_spritesheet__png, true, 640, 360),
 	];
 
 	override public function create()
 	{
 		super.create();
+
+		// gameboy animation
+		spriteDict["gameboy"].animation.add("default", [0, 1], 12);
+		spriteDict["gameboy"].animation.play("default");
+		// setting door frame
+		spriteDict["door"].animation.frameIndex = 0;
 
 		// add sprites
 		for (sprite in spriteDict)
