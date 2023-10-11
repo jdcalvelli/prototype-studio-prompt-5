7092eac97ebc6dd4439d3862126a6f5b1c451f19
JD Calvelli <jdcalvelli@10-18-252-205.dynapool.wireless.nyu.edu>
2023-10-11 12:19:33 -0400

    adding static sprites

--- source/scenes/PlayState.hx
+++ source/scenes/PlayState.hx
@@ -1,21 +1,34 @@
 package scenes;
 
 import flixel.FlxG;
+import flixel.FlxSprite;
 import flixel.FlxState;
-import flixel.text.FlxText;
 import singletons.GameManager;
 import singletons.Messenger;
 
 class PlayState extends FlxState
 {
-	var text = new FlxText(0, 0, 0, GameManager.Instance.testVar, 64);
+	// static sprites dict
+	var spriteDict:Map<String, FlxSprite> = [
+		"background" => new FlxSprite().loadGraphic(AssetPaths.Background__PNG),
+		"room" => new FlxSprite().loadGraphic(AssetPaths.Room__PNG),
+		"desk" => new FlxSprite().loadGraphic(AssetPaths.Desk__PNG),
+		"stuff-on-desks" => new FlxSprite().loadGraphic(AssetPaths.Stuff_On_Desk__PNG),
+		"whiteboard" => new FlxSprite().loadGraphic(AssetPaths.Whiteboard__PNG),
+	];
 
 	override public function create()
 	{
 		super.create();
-		text.screenCenter();
-		add(text);
 
+		// add sprites
+		for (sprite in spriteDict)
+		{
+			sprite.screenCenter();
+			add(sprite);
+		}
+
+		// global signals subscription
 		Messenger.Instance.OnSpace.add(this.onSpaceSignal);
 	}
 
@@ -32,7 +45,5 @@ class PlayState extends FlxState
 	private function onSpaceSignal()
 	{
 		trace("signal bussed");
-		text.text = "signal bussed";
-		text.screenCenter();
 	}
 }
